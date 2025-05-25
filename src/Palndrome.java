cc493ae0bb1740700b55b7188ea3e5ace4db5274
hmkowshal <hm.kowshal@gmail.com>
2025-05-25 20:49:24 +0530

    palindrome checked

---
+++ src/Palndrome.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Palndrome {
+    //function to check for palindrome
+    public static boolean checkIfPalindrome(int inputNum){
+        if(inputNum<0)
+            throw new InputMismatchException();
+        StringBuffer numString = new StringBuffer(String.valueOf(inputNum));
+        StringBuffer reversedNumString = new StringBuffer(String.valueOf(inputNum)).reverse();
+        return numString.compareTo(reversedNumString) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter a number to check if its palindrome: ");
+        try(Scanner sc = new Scanner(System.in)) {
+            int inputNum = sc.nextInt();
+            System.out.printf("%d is %s palindrome",inputNum,checkIfPalindrome(inputNum)?"a":"not a");
+        }catch(InputMismatchException e){
+            System.out.println("Please enter positive numbers only");
+        }
+    }
+}
